1f7452097c74297c0e9f910ee353c9eceaa70b55
abubakar751 <abubakar731743@gmail.com>
2025-01-24 20:30:37 +0530

    HQL Query

--- HibernateFullCode/src/main/java/com/hibernate/hql/HQLExample.java
+++ HibernateFullCode/src/main/java/com/hibernate/hql/HQLExample.java
@@ -2,14 +2,17 @@ package com.hibernate.hql;
 
 import java.util.List;
 
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
 import com.hibernate.life_cycle.Employee;
 
 public class HQLExample {
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		Configuration configuration = new Configuration();
 		configuration.configure("com/hibernate/life_cycle/config.xml");
@@ -21,10 +24,22 @@ public class HQLExample {
 		Query q=session.createQuery(query);
 		q.setParameter("salary", 400000.0);
 		List<Employee> list =q.list();
+		Transaction transaction = session.beginTransaction();
 		for(Employee list2:list) {
 		System.out.println(list2.getEmpName());
 		
 		}
+		System.out.println("____________________________________________________________");
+		
+		String query2 = "delete from Employee where empId=1";
+		Query<?> create = session.createQuery(query2);
+		int executeUpdate = create.executeUpdate();
+
+		
+		transaction.commit();
+		System.out.println("Deleted");
+		System.out.println(executeUpdate);
+		
 		session.close();
 		factory.close();
 	}
