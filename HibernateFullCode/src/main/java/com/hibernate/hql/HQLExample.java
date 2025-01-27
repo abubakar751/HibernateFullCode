170fd8cff11f194feaaf8d281e599bac3ab1c083
abubakar751 <abubakar731743@gmail.com>
2025-01-27 18:29:42 +0530

    Hql

--- HibernateFullCode/src/main/java/com/hibernate/hql/HQLExample.java
+++ HibernateFullCode/src/main/java/com/hibernate/hql/HQLExample.java
@@ -2,7 +2,6 @@ package com.hibernate.hql;
 
 import java.util.List;
 
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -15,31 +14,34 @@ public class HQLExample {
 	@SuppressWarnings("deprecation")
 	public static void main(String[] args) {
 		Configuration configuration = new Configuration();
-		configuration.configure("com/hibernate/life_cycle/config.xml");
+		configuration.configure("com/hibernate/one_to_one/config.xml");
 		SessionFactory factory = configuration.buildSessionFactory();
 		Session session = factory.openSession();
 
-		String query = "from Employee where empSalary=:salary";
-		
-		Query q=session.createQuery(query);
-		q.setParameter("salary", 400000.0);
-		List<Employee> list =q.list();
-		Transaction transaction = session.beginTransaction();
-		for(Employee list2:list) {
-		System.out.println(list2.getEmpName());
-		
-		}
-		System.out.println("____________________________________________________________");
-		
-		String query2 = "delete from Employee where empId=1";
-		Query<?> create = session.createQuery(query2);
-		int executeUpdate = create.executeUpdate();
+	//	String query = "from Employee where empSalary=:salary";
 
 		
+	//	  Query q=session.createQuery(query); q.setParameter("salary", 400000.0);
+		 // List<Employee> list =q.list(); 
+		  Transaction transaction =session.beginTransaction();
+			/*
+			 * for(Employee list2:list) { System.out.println(list2.getEmpName());
+			 * 
+			 * }
+			 */
+		 
+
+		System.out.println("____________________________________________________________");
+		String query2 = "SELECT q.questionId, q.question FROM Question q INNER JOIN q.answer a";
+		Query<Object[]> create = session.createQuery(query2, Object[].class);
+		List<Object[]> results = create.list();
+
+		for (Object[] row : results) {
+		    System.out.println("Question ID: " + row[0] + ", Question: " + row[1]);
+		}
 		transaction.commit();
-		System.out.println("Deleted");
-		System.out.println(executeUpdate);
 		
+
 		session.close();
 		factory.close();
 	}
